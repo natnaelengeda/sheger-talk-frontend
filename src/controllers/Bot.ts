d768d4ce1d14be9284b62292569654900a12db49
NattyEngeda <nattynengeda@gmail.com>
2023-08-10 18:35:42 +0300

    Primary Commit

--- src/controllers/Bot.ts
+++ src/controllers/Bot.ts
@@ -1,123 +1,31 @@
-import { Context, Telegraf, session } from "telegraf";
-import { message } from 'telegraf/filters';
-
+import TelegramBot from 'node-telegram-bot-api';
 import dotenv from 'dotenv';
 
-interface SessionData {
-  messageCount: number;
-  // ... more session data go here
-}
-
-// Define your own context type
-interface MyContext extends Context {
-  session?: SessionData;
-  // ... more props go here
-}
-
 dotenv.config();
+const token = process.env.TELEGRAM_TOKEN!;
+const bot = new TelegramBot(token, { polling: true });
 
-// Define your own context type
-interface MyContext extends Context {
-  myProp?: string
-  myOtherProp?: number
-}
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
 
-const bot = new Telegraf<MyContext>(process.env.TELEGRAM_BOT_API!);
+  const chatId = msg.chat.id;
+  const resp = match![1]; // the captured "whatever"
 
-bot.use(session());
-
-bot.start((ctx) => {
-
-  ctx.reply('Welcome to Sheger Talk Bot');
-  bot.telegram.sendMessage(ctx.chat.id, 'Choose Your Language 👇', {
-    reply_markup: {
-      keyboard: [
-        [
-          { text: "🇺🇸 English" },
-          { text: "🇪🇹 አማርኛ" },
-        ]
-      ],
-      resize_keyboard: true,
-    }
-  });
-
-  bot.on("message", async (ctx) => {
-    // set a default value
-    ctx.session ??= { messageCount: 0 };
-    ctx.session.messageCount++;
-    console.log("First Message");
-  });
-
-});
-
-bot.hears('🇺🇸 English', (ctx) => {
-  const letsStartString = 'Signup To Sheger Talk and talk to Habeshas form all over 🇪🇹 Ethiopia \n\n' +
-    'Get Your Friends Now 👇 \n\n' +
-    '🇪🇷 Eritrea will be added soon \n'
-
-  bot.telegram.sendMessage(ctx.chat.id, letsStartString, {
-    reply_markup: {
-      keyboard: [
-        [
-          { text: 'Lets Start 👇' }
-        ]
-      ],
-      resize_keyboard: true,
-    }
-  });
-
-});
-
-bot.hears('Lets Start 👇', (ctx) => {
-  const confirmationString = '❗️ Remember that on the internet people can impersonate others \n\n\n The bot does not ask for personal data and does not identify users by any documents. \n\n Enjoy and Don\'t forget to have fun and share with your friends 👇 \n\n';
-
-  bot.telegram.sendMessage(ctx.chat.id, confirmationString, {
-    reply_markup: {
-      keyboard: [
-        [
-          { text: 'Continue ✌️' },
-        ]
-      ],
-      resize_keyboard: true,
-      one_time_keyboard: true,
-    }
-  });
-
-
-
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
 });
 
 
-bot.hears('Continue ✌️', (ctx) => {
-  const ageString = 'Your age?';
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
 
-  bot.telegram.sendMessage(ctx.chat.id, ageString);
-
-  bot.on("message", async (ctx) => {
-    // set a default value
-    ctx.session ??= { messageCount: 0 };
-    ctx.session.messageCount++;
-
-    if (ctx.session.messageCount == 1) {
-      console.log("This Is The Age");
-      console.log(ctx.message.text);
-      await ctx.reply(`Your Age is this now.`);
-    }
-
-  });
-
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
 });
 
-
-
-
-// Launch the Bot
-bot.launch();
-
-
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
 // Export the Bot
 module.exports = bot;
