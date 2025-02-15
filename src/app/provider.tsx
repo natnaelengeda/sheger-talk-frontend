94a97cebf6adfd68e7f28861df76df2929b857f0
nattyengeda <nattynengeda@gmail.com>
2025-02-15 17:44:07 +0300

    Add Firebase

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect } from "react";
 
 // Socket
 import { SocketProvider } from "@/context/SocketProvider";
@@ -7,9 +8,15 @@ import { SocketProvider } from "@/context/SocketProvider";
 import { Provider } from 'react-redux';
 import { persistor, store } from "./store";
 import { PersistGate } from 'redux-persist/integration/react';
+import { analytics, firebase } from "@/utils/firebase";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const url = process.env.NEXT_PUBLIC_API_URL || "";
+
+  useEffect(() => {
+    firebase;
+    analytics;
+  }, []);
 
   return (
     <Provider
